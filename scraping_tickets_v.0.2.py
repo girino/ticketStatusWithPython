468d5dbe2faa3ad96e2d749d21e6df00ecc4efc1
Girino Vey <girino@Girinos-Mac-mini.local>
2018-09-02 00:20:15 -0300

    ful lrefactor, pretty print, gets date voted and much more

--- scraping_tickets_v.0.2.py
+++ scraping_tickets_v.0.2.py
@@ -5,76 +5,117 @@
 from bs4 import BeautifulSoup
 from os.path import expanduser
 import platform, os, requests
+import argparse
+
+# constants
 
 
 class userConfig():
-    #print user OS (Linux for Linux, Darwin for Mac and Windows for Windows)
-    def user_os(self):
-        return platform.system()
+    def __init__(self, path, order, full_txid):
+        self.path = self.find_splitticketbuyer_folder(path)
+        self.order = order
+        self.full_txid = full_txid
     #get the user home folder (C:\Users\name)
     def home_folder(self):
-        home = expanduser('~')
-        return home
-    #validate the split folder
-    def split_folder_validator(self):
-        if os.path.exists(self.split_ticket_folder()) == False:
-            return ('Split ticket folder not found')
-        else:
-            True
+        return expanduser('~')
     #return a list of all split tickets on folder
     def all_split_tickets(self):
-        folder = os.listdir(self.split_ticket_folder())
-        return folder
+        return os.listdir(self.path)
     #return a list of split ticket url
-    def split_ticket_url(self):
-        stu = []
-        for split in self.all_split_tickets():
-            stu.append(ticket.explorer_url(ticket) + split)
-        return stu
+    def split_ticket_txid(self):
+        return [split for split in self.all_split_tickets()]
     #complete split ticket folder
-    def split_ticket_folder(self):
-        complete_folder = user.home_folder() + ticket.default_split_folder(ticket)
-        return complete_folder
+    def find_splitticketbuyer_folder(self, path):
+        dsf = '/data/sessions'
+        if path:
+            if os.path.exists(path + dsf):
+                return path + dsf
+            else:
+                raise Exception('Cannot find path: ' + path)
+        else:
+            dsf = '.splitticketbuyer' + dsf
+            paths = [dsf, self.home_folder() + '/' + dsf]
+            for p in paths:
+                if os.path.exists(p):
+                    return p
+            raise Exception('Cannot find splitticketbuyer data path. Try passing it as a parameter')
+    def get_tickets(self):
+        ret = [ticket(id) for id in self.split_ticket_txid()]
+        ret = sorted(ret, key=ticket.get_date)
+        if self.order != 'date':
+            ret = sorted(ret, key=ticket.get_status)
+        return ret
+    def print_tickets(self):
+        header =    '++====================================+============+============+========++'
+        separator = '++------------------------------------+------------+------------+--------++'
+        if self.full_txid:
+            mask = '|| %64s | %10s | %10s | %6s ||'
+            indent = (" " * 60)
+        else:
+            mask = '|| %34s | %10s | %10s | %6s ||'
+            indent = (" " * 30)
+
+        print(header)
+        print( mask % ('TXID' + indent,'DATA' + (" " * 6), 'DATA VOTO ','STATUS'))
+        print(header)
+
+        tickets = self.get_tickets()
+        for t in tickets:
+            if t != tickets[0]:
+                print(separator)
+            txid = t.get_txid()
+            if not self.full_txid:
+                txid = txid[:31] + '...'
+            print( mask % (txid, t.get_date(), t.get_date_voted(), t.get_status()) )
+        print(header)
 
 class ticket():
-    def __init__(self,url):
-        self.url = url
-    #default split ticket folder
-    def default_split_folder(self):
-        dsf = ('\.splitticketbuyer\data\sessions')
-        return dsf
+    def __init__(self, txid):
+        self.txid = txid
+        self.url = self.explorer_url() + txid
+        page = requests.get(self.url)
+        self.parsed = BeautifulSoup(page.content, 'html.parser')
+        self.status = (self.parsed.find_all('td')[5].get_text().split()[0])
+        self.date_voted = ''
+        if self.status == 'Voted':
+            self.date_voted = self.get_vote().get_date()
     #return a default Decred block explorer
     def explorer_url(self):
-        eurl = ('https://explorer.dcrdata.org/tx/')
-        return eurl
-    #request info from url
-    def page(self):
-        page = requests.get(self.url)
-        return BeautifulSoup(page.content, 'html.parser')
+        return 'https://explorer.dcrdata.org/tx/'
+    def get_txid(self):
+        return self.txid
     #return ticket status
-    def status(self):
-        return (self.page().find_all('td')[5].get_text().split()[0])
+    def get_status(self):
+        return self.status
+    # returns the ticket that voted this one. Not cached, beware
+    def get_vote(self):
+        return ticket(self.parsed.find_all('td')[5].find_all('a', href=True)[0]['href'].split('/')[2])
     # return ticket data
-    def data(self):
-        if self.status() == 'live':
-            return (self.page().find_all('td')[17].get_text().split()[1])
-        elif self.status() == 'voted':
-            return (self.page().find_all('td')[1].get_text().split()[1])
+    def get_date(self):
+        if self.status == 'live':
+            return (self.parsed.find_all('td')[17].get_text().split()[1])
+        elif self.status == 'voted':
+            return (self.parsed.find_all('td')[1].get_text().split()[1])
         else:
-            return (self.page().find_all('td')[13].get_text().split()[1])
+            return (self.parsed.find_all('td')[13].get_text().split()[1])
+    def get_date_voted(self):
+        return self.date_voted
 
-print('\n\n\n')
-print('________________________________________________________')
-print('| Check files in folder \.splitticketbuyer\data\sessions|')
-print('| and get online status of split ticket.          Enjoy.|')
-print('|_______________________________________________________|\n')
 
-user = userConfig()
+parser = argparse.ArgumentParser(description='Check Split Ticket status.')
+parser.add_argument('path', metavar='F', type=str, nargs='?', default=False,
+                    help='path where to find the splitticketbuyer data files. (default to ~/.splitticketbuyer)')
+parser.add_argument('--date', dest='order', action='store_const',
+                    const='date', default='status',
+                    help='sorts by date (default: sorts by status then by date)')
+parser.add_argument('--full', dest='full_txid', action='store_const',
+                    const=True, default=False,
+                    help='prints full txid (might be larger than screen).')
 
-for x in user.split_ticket_url():
-    print(x)
-    print('Ticket data: ', ticket(x).data())
-    print('Status     : ', ticket(x).status())
-    print('')
+args = parser.parse_args()
 
-input('\nPress ENTER to close.')
+user = userConfig(args.path, args.order, args.full_txid)
+user.print_tickets()
+
+
+raw_input('\nPress ENTER to close.')
